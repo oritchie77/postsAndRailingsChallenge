80a906df72207d94614fb59b4a4df2680bc29387
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-01 22:03:55 +0000

    added forms to html

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $numberOfPostsFromLength = generatePostsFromRails($numberOfRailsFromLength);
 
 <head>
     <title>Posts & Railings</title>
-
+    <link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 
 <body>
@@ -21,11 +21,23 @@ $numberOfPostsFromLength = generatePostsFromRails($numberOfRailsFromLength);
     <h1>Posts & Railings Calculator</h1>
     <div>
         <h2>From Length to number needed</h2>
+        <form>
+            <lable for="lengthNeeded">length needed:</lable><input id="lengthNeeded" type="number" placeholder="Unit (m)">
+            <input type="submit">
+        </form>
         <?= "<p>Length: " . $length . "<br><br>Rails: " . $numberOfRailsFromLength . "<br> Posts: " . $numberOfPostsFromLength; ?>
     </div>
 
     <div>
         <h2>From Supplies to Length possible</h2>
+        <form>
+            <lable for="posts">Posts available:</lable>
+            <input id="posts" type="number">
+            <br>
+            <lable for="rails">Rails available:</lable>
+            <input id="rails" type="number">
+            <input type="submit">
+        </form>
         <?= "<p>Posts: ". $numberOfPosts . "<br>Rails:" . $numberOfRails . "<br><br>Length of fence: " . $lengthFromSupplies . "</p>"; ?>
     </div>
 </body>
