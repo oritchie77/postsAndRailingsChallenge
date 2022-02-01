5668c7e3e4602c730f40f3deec96e3004457dbcf
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-02-01 21:42:49 +0000

    added all functions - working but still to be tested

--- functions.php
+++ functions.php
@@ -1,1 +1,38 @@
 <?php
+
+
+function generateRailsFromGivenLength(int $length): int {
+    $postWidth = 0.1;
+    $railingLength = 1.5;
+
+    return $length / $railingLength;
+
+}
+
+function generatePostsFromRails(int $rails): int {
+    return $rails + 1;
+}
+
+function generateLengthFromSupplies(int $posts, int $rails): float {
+
+    if ($posts % 2 == 0) {
+        $posts -- ;
+    }
+
+    if($rails > $posts) {
+        $rails = $posts - 1;
+    } elseif($posts > $rails) {
+        $posts = $rails + 1;
+    }
+
+    $postWidth = 0.1;
+    $railingLength = 1.5;
+
+    return ($postWidth * $posts) + ($railingLength * $rails);
+}
+
+//function generate pols from given lenght
+//
+//function generate lengght form posts
+//
+//functon generate lenght from pols
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+require_once 'functions.php';
+$length = 3.3;
+$numberOfPosts = 40;
+$numberOfRails = 41;
+
+$lengthFromSupplies = generateLengthFromSupplies($numberOfPosts, $numberOfRails);
+$numberOfRailsFromLength = generateRailsFromGivenLength($length);
+$numberOfPostsFromLength = generatePostsFromRails($numberOfRailsFromLength);
+?>
+
+<html lang="en-GB">
+
+<head>
+    <title>Posts & Railings</title>
+
+</head>
+
+<body>
+
+    <h1>Posts & Railings Calculator</h1>
+    <div>
+        <h2>From Length to number needed</h2>
+        <?= "<p>Length: " . $length . "<br><br>Rails: " . $numberOfRailsFromLength . "<br> Posts: " . $numberOfPostsFromLength; ?>
+    </div>
+
+    <div>
+        <h2>From Supplies to Length possible</h2>
+        <?= "<p>Posts: ". $numberOfPosts . "<br>Rails:" . $numberOfRails . "<br><br>Length of fence: " . $lengthFromSupplies . "</p>"; ?>
+    </div>
+</body>
+
+
+</html>
